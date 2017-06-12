b992380545efff0c7c0ba8e3475d228c3dba785b
Lucas Isla <lisla@purdue.edu>
2017-06-12 16:23:26 -0400

    Expand li to show buttons

--- app.js
+++ app.js
@@ -57,10 +57,14 @@ class App{
     item
       .querySelector('.artist-name')
       .textContent = artist.name
-    
+
+    item
+      .querySelector('i.fa')
+      .addEventListener('click', this.toggleButtons.bind(this, item))
+
     item
       .querySelector('button.fav')
-      .addEventListener('click', this.favArtist.bind(this))
+      .addEventListener('click', this.favArtist.bind(this, artist))
 
     item
       .querySelector('button.remove')
@@ -74,22 +78,40 @@ class App{
       .querySelector('button.down')
       .addEventListener('click', this.moveDown.bind(this, artist))
 
-    // item 
-    //   .querySelector('button.songs')
-    //   .addEventListener('click', this.showSongs.bind(this, artist))
     return item
   }
 
-  favArtist(ev){
+  toggleButtons(item, ev){
 
+    const actions = item.querySelector('.actions')
+    const icon = ev.target
+      
+    if (actions.style.display === 'none'){
+      actions.style.display = 'inline'
+      icon.classList.remove('fa-plus-circle')
+      icon.classList.add('fa-minus-circle')
+    }
+    else{
+      actions.style.display = 'none'
+      icon.classList.remove('fa-minus-circle')
+      icon.classList.add('fa-plus-circle')
+    }
+  }
+
+  favArtist(artist, ev){
+    const b = ev.target
+    const element = b.closest('li')
+
+    element.classList.toggle('fav')
+    artist.fav = !artist.fav
   }
 
   removeArtist(ev){
-
+    
   }
 
   moveUp(artist, ev){
-
+    
   }
 
   moveDown(artist, ev){
